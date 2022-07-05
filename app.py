f5084062047f534e653ba45d164421983c3f4f90
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-05 22:33:21 +0100

    add route

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return"<h1>Hello Igor</h1>"
 
 if __name__ =="__main__":
     app.run(debug=True, port=4000) # set port 4000
