5bada8c06c8243f0b0a1359e7b35fe0907d3c5a5
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-05 22:35:31 +0100

    add dynamic route

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return"<h1>Hello Igor</h1>"
+    return"<h1 style='color:red'>Hello Igor</h1>"
+
+
+# Dynamic Route
+@app.route('/students/<name>')
+def student(name):
+    return f"<h1 style='color:red'>Profilepage for {name.capitalize()}</h1>"
+
 
 if __name__ =="__main__":
     app.run(debug=True, port=4000) # set port 4000
