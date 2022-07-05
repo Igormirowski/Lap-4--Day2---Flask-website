d7bc5c5f77852b61052128540318bd877ff34fd8
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-05 22:53:38 +0100

    create signup reoute and page html

--- app.py
+++ app.py
@@ -18,6 +18,29 @@ def info():
     return "<h1>We are currently in lap4</h1>"
 
 
+#signup page
+@app.route('/signup')
+def signup_form():
+    return render_template("signup.html")
+
+# #welcome page
+# @app.route('/welcome')
+# def welcome():
+#     first = request.args.get('first')
+#     last = request.args.get('last')
+#     return render_template("welcome.html", first=first, last=last)
+
+# #welcome page access logi data 
+
+
+# # handle 404
+#  @app.errorhandler(404)
+#  def page_not_found(e)
+#  path = request.path
+#     return render_template('error/404.html',path=path),404
+
+
+
 
 if __name__ =="__main__":
     app.run(debug=True, port=4000) # set port 4000
